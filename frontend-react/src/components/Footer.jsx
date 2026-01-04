3069e21bf47ff8da48dc880b913a78f7280b72c7
Gunjan097 <gunjanmaheshwari09@gmail.com>
2026-01-04 21:51:53 +0530

    Home Page

---
+++ frontend-react/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+    <footer className=' footer my-3'>
+        <hr className='border-bottom'/>
+        <p className='text-center'>&copy; 2026 Stock Prediction Portal. All rights reserved.</p>
+    </footer>
+    </>
+  )
+}
+
+export default Footer
---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Button from './Button'
+const Main = () => {
+  return (
+    <>
+    <div className="container ">
+        <div className="text-center p-5 bg-light-dark rounded-3 my-3">
+
+        <h1>Stock Prediction Portal</h1>
+        <p>Welcome to the Stock Prediction Portal. Here you can analyze and predict stock market trends using advanced machine learning algorithms. Data is processed through a neural network model trained on historical stock data.</p>
+        <Button text="Login" class='btn btn-outline-info'/>
+      </div>
+        </div>
+    </>
+  )
+}
+
+export default Main
